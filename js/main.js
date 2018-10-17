ab14286f3e96bac38ac9586174965bd5524bf0bb
Martin Kovac <martin_kovac@outlook.com>
2018-10-17 08:30:35 +0100

    Add Square to canvas

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
+import Square from './sprites/Square.js'
+
 window.onload = () => {
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
-
+  const square = new Square(0, 0);
+  let x = 0, y = 0;
   
   drawFrame();
 
@@ -13,15 +16,27 @@ window.onload = () => {
     window.requestAnimationFrame(drawFrame);
     
   }
-
+  
+  
   function draw() {
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(30, 0);
-    ctx.lineTo(30, 30);
-    ctx.lineTo(0, 30);
-    ctx.closePath();
-    ctx.stroke();
+    
+    square.draw(ctx);
+    //square.draw(ctx);
+    // ctx.translate(50, 50);
+    // ctx.fillRect(0,0,100,100);
+    // ctx.setTransform(1, 0, 0, 1, 0, 0);
+    // const path = new Path2D('M 100,100 h 50 v 50 h 50');
+    // ctx.rotate(30 * Math.PI / 180);
+    // ctx.stroke(path);
+    // ctx.setTransform(1, 0, 0, 1, 0, 0);
+    // const path1 = new Path2D(`M 50,50 h 50 v 50 h 50`);
+    // ctx.rotate(40 * Math.PI / 180);
+    // //ctx.translate(x, 50);
+    // ctx.stroke(path1);
+    // ctx.setTransform(1, 0, 0, 1, 0, 0);
+    
+    
+    
   }
   
 }
