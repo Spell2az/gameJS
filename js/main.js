cd34c2ddca82a3a9a6175a8b71d34a2106d8890b
Spell2az <martin_kovac@outlook.com>
2019-01-29 16:01:20 +0000

    Add asteroid maker

--- js/main.js
+++ js/main.js
@@ -1,22 +1,48 @@
 const WIDTH = 800;
 const HEIGHT = 600;
+const MAX_NUMBER_OF_ASTEROIDS = 25;
+//const img = new Image(100, 100);
+//img.src = "../img/asteroid_blue.png";
 
-window.onload = () => {
+
+
+window.onload= () => {
 
   const canvas = document.getElementById('canvas');
   
   const ctx = canvas.getContext('2d');
   
   const player = new Player({x:WIDTH/2, y: HEIGHT/2});
+  const asteroids = [];
   
+  const makeAsteroids = () => {
+    while(asteroids.length < MAX_NUMBER_OF_ASTEROIDS){
+      const positionY = getRandomInt(0, HEIGHT);
+      const positionX = getRandomInt(0, WIDTH);
+      const distance = getDistanceBetweenTwoPoints([player.position.x, player.position.y],
+        [positionX, positionY])
+        console.log(distance);
+      if( distance > 100){
+          const velocityX = getRandom(- 0.5, 0.5);
+          const velocityY = getRandom(- 0.5, 0.5);
+          const angle = Math.PI * getRandom(0, 2);
+          const angleVelocity = getRandom(-0.1, 0.1);
+          const radius = getRandomInt(25, 75);
+          asteroids.push(new Asteroid({x: positionX ,y: positionY}, angle, {x: velocityX, y: velocityY}, angleVelocity, radius))
+        }
+     
+    }
+  }
  
   drawFrame();
 
   function drawFrame() {
+    makeAsteroids();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-     
     player.draw(ctx);
-    
+    ctx.fillStyle = "red";
+    //ctx.fillRect(100,100,100,100);
+   asteroids.forEach(asteroid => asteroid.draw(ctx))
     window.requestAnimationFrame(drawFrame);
     
   }
